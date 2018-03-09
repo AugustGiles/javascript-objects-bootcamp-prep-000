8d8c3e85bd0bd7a712980e6459d4dd8cb6d1c687
Taylor August Giles <augustgiles8@gmail.com>
2018-03-09 07:55:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,5 @@ const playlist = {"Paul Simon": "Graceland"};
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
-updatePlaylist(playlist, "Phil Ochs", )
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
+
